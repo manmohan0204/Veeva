ef97793b3636fed2dfd86ac1b5e0c1f8ee7b2344
manmohan0204 <engr.manmohandash@gmail.com>
2024-08-26 12:41:56 +0530

    Mandatory Veeva Assignmnet Completed

---
+++ src/main/java/coreUtility/Exceptions/PageNotLoadedException.java
@@ -0,0 +1,21 @@
+package coreUtility.Exceptions;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * <b>Name :</b> PageNotLoadedException </br>
+ * <b>Description : </b> This Class is responsible to handle PageNot Loaded related Exception</br>
+ *
+ * @author <i>Manmohan Dash</i>
+ */
+public class PageNotLoadedException extends LoggedRuntimeException {
+
+
+    public PageNotLoadedException(String message, WebDriver driver) {
+        super(message, driver);
+    }
+
+    public PageNotLoadedException(String message, Throwable throwable, WebDriver driver) {
+        super(message, throwable, driver);
+    }
+}
---
+++ src/main/java/coreUtility/TestComponent/DriverFactory.java
@@ -0,0 +1,50 @@
+package coreUtility.TestComponent;
+
+import coreUtility.AbstractComponent.AbstractPage;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * <b>Name :</b> DriverFactory </br>
+ * <b>Description : </b> This DriverFactory Class responsible for deciding to invoke driver based on user choice</br>
+ *
+ * @author <i>Manmohan Dash</i>
+ */
+public class DriverFactory {
+
+    private final static Logger _log = Logger.getLogger(String.valueOf(AbstractPage.class));
+    private static final DriverFactory instance = new DriverFactory();
+    private static final ThreadLocal<WebDriver> threadLocal = new ThreadLocal<WebDriver>();
+
+    private DriverFactory() {
+
+    }
+
+    public static DriverFactory getInstance() {
+        return instance;
+    }
+
+    public WebDriver getDriver(String type) throws Exception {
+        if (threadLocal.get() == null) {
+            switch (type) {
+                case "CHROME":
+                    threadLocal.set(new ChromeDriver());
+                    break;
+                case "FIREFOX":
+                    threadLocal.set(new FirefoxDriver());
+                    break;
+                case "EDGE":
+                    threadLocal.set(new EdgeDriver());
+                    break;
+                default:
+                    throw new Exception("Provided Browser Type: '" + type + "' is not supported or" +
+                            " should not be empty, please specify the proper browser name");
+            }
+        }
+        return threadLocal.get();
+    }
+
+}
